7de3d357178b100901e5a2316b5217c3f73fc8b1
Jianqoq <120760306+Jianqoq@users.noreply.github.com>
2023-03-09 00:38:09 -0500

    Update model.py

--- model.py
+++ model.py
@@ -1,14 +1,11 @@
-from layers2 import MatMul, Softmax_Cross_entropy, WordEmbed, WordEmbedDot, SigmoidWithLoss
-import numpy as np
-import time
+from layers import MatMul, Softmax_Cross_entropy, WordEmbed, WordEmbedDot, SigmoidWithLoss
+import cupy as cp
 
 
-
-
-class SimpleCbow:
+class SimpleCBow:
     def __init__(self, row, cols):
-        W_in = np.random.randn(row, cols).astype('f')
-        W_out = np.random.randn(cols, row).astype('f')
+        W_in = cp.random.randn(row, cols).astype('f')
+        W_out = cp.random.randn(cols, row).astype('f')
         self.layer1 = MatMul(W_in)
         self.layer2 = MatMul(W_in)
         self.layer3 = MatMul(W_out)
@@ -23,17 +20,14 @@ class SimpleCbow:
         self.grads += self.layer3.gradients
 
     def forward(self, context, target):
-
         a = self.layer1.forward(context[:, 0])
         b = self.layer2.forward(context[:, 1])
         c = 0.5 * (a + b)
         prediction = self.layer3.forward(c)
         loss = self.layer4.forward(target, prediction)
-
         return loss
 
     def backward(self, dout=1):
-
         ds = self.layer4.backward(dout)
         da = self.layer3.backward(ds)
         da *= 0.5
@@ -43,8 +37,8 @@ class SimpleCbow:
 
 class CBow:
     def __init__(self, row: int, cols: int, sample_size: int, sample_set):
-        W_in = 0.01*np.random.randn(row, cols).astype('f')
-        W_out = 0.01*np.random.randn(row, cols).astype('f')
+        W_in = 0.01*cp.random.randn(row, cols).astype('f')
+        W_out = 0.01*cp.random.randn(row, cols).astype('f')
         self.negative_sample_set = sample_set
         self.layer1 = WordEmbed(W_in)
         self.layer2 = WordEmbed(W_in)
@@ -59,23 +53,25 @@ class CBow:
             self.weights += i.weights
             self.grads += i.grads
 
-    def forward(self, context, target, batch):
+    def forward(self, context, target, batch) -> cp.ndarray:
         result1 = self.layer1.forward(context[:, 0])
         result2 = self.layer2.forward(context[:, 1])
         h = 0.5*(result2+result1)
         batch_size = len(target)
         score = self.Embedding_dot[0].forward(h, target)
-        correct_label = np.ones(batch_size, dtype="uint8")
-        negative_label = np.zeros(batch_size, dtype="uint8")
+        correct_label = cp.ones(batch_size, dtype="int32")
+        negative_label = cp.zeros(batch_size, dtype="int32")
         loss = self.Sigmoid_loss[0].forward(correct_label, score)
         negative_score = [self.Embedding_dot[index+1].forward(h, batch[:, index]) for index in range(len(self.Embedding_dot) - 1)]
         for index in range(len(self.Sigmoid_loss)-1):
             loss += self.Sigmoid_loss[index + 1].forward(negative_label, negative_score[index])
         return sum(loss)
 
-    def backward(self):
+    def backward(self) -> None:
         dh = 0
         for Sigmoid_loss, Embedding_dot in zip(self.Sigmoid_loss, self.Embedding_dot):
             dscore = Sigmoid_loss.backward()
             dh += Embedding_dot.backward(dscore)
-        return dh
+        dh /= 2
+        self.layer1.backward(dh)
+        self.layer2.backward(dh)
